61ccb226682715977e2f0b65734f1b080b4d61e1
mgutierrezg-simulacroexamen-d6soyvek <mgutierrezg-simulacroexamen-d6soyvek@local>
2018-05-24 00:46:54 +0000

    2

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,3 +1,16 @@
 # Escriba el codigo necesario para que al ejecutar python ejercicio2.py
 # se cree una grafica con una funcion sinusoidal entre 0 y 2pi.
 
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np 
+
+x = np.linspace(0,2*np.pi,1000)
+y = np.sin(x)
+
+plt.plot(x,y,c = "m")
+plt.xlabel("Rad")
+plt.ylabel("FuncSin")
+plt.title("Seno(x)")
+plt.savefig("hola.png")
